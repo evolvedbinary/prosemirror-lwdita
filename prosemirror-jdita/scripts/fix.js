519263ffc0935bce0ce4dd9ec203bb5a40a9cdc0
marmoure <tinky93winky@gmail.com>
2023-12-20 16:10:23 +0100

    [bugfix] adjust the fix script to the latest version

--- prosemirror-jdita/scripts/fix.js
+++ prosemirror-jdita/scripts/fix.js
@@ -3,38 +3,35 @@
  * doesn't change position after adding a new line
  */
 
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// const filenames = [
-//   'prosemirror-view/dist/index.es.js',
-//   'prosemirror-view/dist/index.js',
-// ];
-// let dir = path.join(__dirname, '../..');
-// if (path.basename(dir) !== 'node_modules') {
-//   dir = path.join(dir, 'node_modules');
-// }
+const filenames = [
+  'prosemirror-view/dist/index.cjs',
+  'prosemirror-view/dist/index.js',
+];
+let dir = path.join(__dirname, '../..');
+if (path.basename(dir) !== 'node_modules') {
+  dir = path.join(dir, 'node_modules');
+}
 
-// let view = path.join(dir, 'prosemirror-view/dist');
+let view = path.join(dir, 'prosemirror-view/dist');
 
-// if(!fs.existsSync(view)) {
-//   dir = path.join(dir, '../../node_modules');
-//   view = path.join(dir, 'prosemirror-view/dist');
-// }
-// if(fs.existsSync(view)) {
-//   const line = '!prev.selection.empty && !state.selection.empty && ';
-//   process.stdout.write('This file attempts to fix a problem where the cursor\n');
-//   process.stdout.write('doesn\'t change position after adding a new line\n\n');
-//   process.stdout.write('Patching "prosemirror-view"...\n');
-//   filenames.forEach(filename => {
-//     filename = path.join(dir, filename);
-//     const content = fs.readFileSync(filename, { encoding: 'utf-8' });
-//     fs.writeFileSync(filename, content.replace(line, ''), { encoding: 'utf-8' });
-//   });
-//   process.stdout.write('done.\n');
-// } else {
-//   process.stdout.write('something went wrong, couldn\'t locate the file.\n');
-// }
-
-
-// The file Charaf tried to modify no longer exists
+if(!fs.existsSync(view)) {
+  dir = path.join(dir, '../../node_modules');
+  view = path.join(dir, 'prosemirror-view/dist');
+}
+if(fs.existsSync(view)) {
+  const line = '!prev.selection.empty && !state.selection.empty && ';
+  process.stdout.write('This file attempts to fix a problem where the cursor\n');
+  process.stdout.write('doesn\'t change position after adding a new line\n\n');
+  process.stdout.write('Patching "prosemirror-view"...\n');
+  filenames.forEach(filename => {
+    filename = path.join(dir, filename);
+    const content = fs.readFileSync(filename, { encoding: 'utf-8' });
+    fs.writeFileSync(filename, content.replace(line, ''), { encoding: 'utf-8' });
+  });
+  process.stdout.write('done.\n');
+} else {
+  process.stdout.write('something went wrong, couldn\'t locate the file.\n');
+}
