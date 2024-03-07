50004627422d7b3e08d50b8e8c8bbe4b9443c5d4
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-03-07 16:09:38 +0100

    [test] Create unit test stub f.document.ts, add TODOs

---
+++ prosemirror-jdita/src/tests/document.spec.ts
@@ -0,0 +1,21 @@
+/**
+ * Unit tests for document.ts
+ */
+
+/*
+// Pass an object with "undefined" attributes
+// and test against expected object
+deleteUndefined()
+
+// Pass a JDita document node
+// and test against expected Prosemirror document output
+defaultTravel()
+
+// Pass a JDita node
+// and test against expected Prosemirror output
+travel()
+
+// Pass a JDita object
+// and test against expected JDita transformation output
+document()
+ */
