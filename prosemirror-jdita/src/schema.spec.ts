e995354de532b30555ce9ad857d145c879cfe3c7
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-03-07 13:34:05 +0100

    [doc] Add comment on schema.spec.ts unit test

--- prosemirror-jdita/src/schema.spec.ts
+++ prosemirror-jdita/src/schema.spec.ts
@@ -2,6 +2,13 @@ import { assert } from 'chai';
 import { travel } from './schema';
 import { TextNode, BaseNode } from 'jdita';
 
+/**
+ * Unit test for `travel()`
+ * on `TextNode`
+ *
+ * @privateRemarks
+ * Do we need tests for other NodeTypes?
+ */
 describe('Schema', () => {
   it('Text node', () => {
     assert.deepEqual(travel(TextNode as unknown as typeof BaseNode, console.log), { attrs: { parent: { default: '' } }} as any);
