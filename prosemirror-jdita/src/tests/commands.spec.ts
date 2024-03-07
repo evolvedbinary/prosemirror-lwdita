2dc8657c1c1bc90476f3b9053cc18b8aa8a931ca
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-03-07 13:35:11 +0100

    [test] Create unit test stub f.command.ts, add TODOs

---
+++ prosemirror-jdita/src/tests/commands.spec.ts
@@ -0,0 +1,86 @@
+/**
+ * Unit tests for command.ts
+ */
+
+/*
+// Pass all 12 NodeTypes from the schema
+// and test against their expected returned nodes
+createNode()
+
+// Pass sample NodeType tree
+// and test against expected node object
+createNodesTree()
+
+// Pass a NodeType OL or UL
+// and test against expected command
+insertNode()
+
+// Pass NodeType, InputContainer,
+// an EditorState, and a transaction object
+// and test against expected command
+insertImage()
+
+// Pass a NodeType
+// and test against expected node index number
+canCreateIndex()
+
+// Pass a NodeType and test against expected Boolean
+canCreate()
+
+// Pass a ResolvedPos object
+// and test against expected list of NodeTypes
+defaultBlocks()
+
+// Pass a ResolvedPos object and an allowed NodeType
+// and test against the expected NodeType
+defaultBlockAt()
+
+// Pass a transaction object
+// and test against expected Boolean
+enterEOL()
+
+// Pass a transaction object
+// and test against expected transaction object
+deleteEmptyLine()
+
+// Pass a transaction object
+// and test against expected transaction of enterEOL()
+enterEmpty()
+
+// Pass a transaction object
+// and test against expected Boolean
+enterSplit()
+
+// Pass a transaction object
+// and test against expected Boolean
+isEOL()
+
+// Pass a transaction object
+// and test against expected Boolean
+isEmpty()
+
+// Pass a transaction object
+// and test against expected Boolean
+isPrevEmpty()
+
+// Pass a transaction object
+// and test against expected number
+getDepth()
+
+// Pass a transaction object
+// and test against expected number
+getPrevDepth()
+
+// Pass a ResolvedPos object
+// and test against expected array of NodeTypes
+getTree()
+
+// Pass an EditorState object, a transaction object,
+// a ResolvedPos object
+// and test against expected Boolean
+enterPressed()
+
+// Pass an EditorState object and a MarkType
+// and test against expected Boolean
+hasMark()
+*/
