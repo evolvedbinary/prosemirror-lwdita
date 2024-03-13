8fdd5deb435a4a5502e7d20ea1bdc3c5b11e7b1a
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-03-13 13:17:28 +0100

    [test] Create unit test for getDomNode(), dom.ts

--- prosemirror-jdita/src/tests/dom.spec.ts
+++ prosemirror-jdita/src/tests/dom.spec.ts
@@ -1,10 +1,33 @@
+import { assert } from 'chai';
+import { getDomNode } from '../dom';
+
 /**
  * Unit tests for dom.ts
+ *
+ * getDomNode()
+ * Pass a JDita node name and test against expected HTML node name
  */
+describe('getDomNode()', () => {
+  let domNode: string;
 
-/*
-// Pass a JDita node name
-// and test against expected HTML node name
-getDomNode()
-*/
+  describe('when passed a JDita node name "topic"', () => {
+    it('returns the according HTML node name "article"', () => {
+      domNode = getDomNode('topic', 'doc');
+      assert.deepEqual(domNode, 'article');
+    });
+  });
 
+  describe('when passed a JDita node name "title"', () => {
+    it('returns the according HTML node name "h1"', () => {
+      domNode = getDomNode('title', 'topic');
+      assert.deepEqual(domNode, 'h1');
+    });
+  });
+
+  describe('when passed a JDita node name "media-source"', () => {
+    it('returns the according HTML node name "source"', () => {
+      domNode = getDomNode('media-source', 'video');
+      assert.deepEqual(domNode, 'source');
+    });
+  });
+});
