e9e3188c3df26c86b3f8bb95335f809581ebea97
marmoure <tinky93winky@gmail.com>
2024-03-13 15:32:53 +0100

    [test] defaultNodeAttrs schema unit test

--- prosemirror-jdita/src/tests/schema.spec.ts
+++ prosemirror-jdita/src/tests/schema.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { defaultToDom, getChildren, getDomAttr, travel } from '../schema';
+import { defaultNodeAttrs, defaultToDom, getChildren, getDomAttr, travel } from '../schema';
 import { TextNode, BaseNode, TopicNode, DdNode, nodeGroups } from 'jdita';
 
 /**
@@ -27,7 +27,7 @@ describe('getChildren', () => {
     const type = DdNode.childTypes;
     const children = getChildren(type);
     // %list-blocks are p  ul  ol  dl  pre  audio  video  simpletable  fig  note  data
-    assert.deepEqual(children, nodeGroups['%list-blocks']);
+    assert.deepEqual(children, ['p', 'ul', 'ol', 'dl', 'pre', 'audio', 'video', 'simpletable', 'fig', 'note', 'data']);
   });
 });
 
@@ -41,10 +41,23 @@ describe('defaultToDom', () => {
 });
 
 describe('getDomAttr', () => {
-  it.only('Id dom attribute for topic', () => {
+  it('Id dom attribute for topic', () => {
     const nodeName = 'topic';
     const attrs = "id";
     const domAttrs = getDomAttr(nodeName, attrs);
     assert.equal(domAttrs, "data-j-id");
+  });
+});
+
+describe('defaultNodeAttrs', () => {
+  it('should return default node attributes', () => {
+    const attrs = ['attr1', 'attr2', 'attr3'];
+    const result = defaultNodeAttrs(attrs);
+    const expected = {
+      attr1: { default: '' },
+      attr2: { default: '' },
+      attr3: { default: '' },
+    };
+    assert.deepEqual(result, expected);
   });
 });
