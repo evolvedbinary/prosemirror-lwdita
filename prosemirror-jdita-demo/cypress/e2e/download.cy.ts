fd0437a3b3c561201c8941e024c8ea2896e2cb34
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-05-08 17:05:17 +0200

    [test] Update Cypress test data for mocking JDITA

--- prosemirror-jdita-demo/cypress/e2e/download.cy.ts
+++ prosemirror-jdita-demo/cypress/e2e/download.cy.ts
@@ -8,8 +8,10 @@ describe('download a file', () => {
     .readFile('cypress/downloads/Test_File.json');
   })
 
+  // TODO: For the time being we are testing a JDITA download.
+  // Once the feature of downloading XDita is completed, change`mockJdita` test data to XDita.
   it('downloads the file correctly', () => {
-    const mockJdita = `{"nodeName":"document","children":[{"nodeName":"topic","attributes":{"id":"program"},"children":[{"nodeName":"title","children":[{"nodeName":"text","content":"Test File 2"}]},{"nodeName":"body","children":[{"nodeName":"section","children":[{"nodeName":"p","children":[{"nodeName":"text","content":"A test paragraph."}]}]}]}]}]}`
+    const mockJdita = `{"nodeName":"document","attributes":{},"children":[{"nodeName":"topic","attributes":{"parent":"doc","id":"program"},"children":[{"nodeName":"title","attributes":{"parent":"topic"},"children":[{"nodeName":"text","content":"Test File 2"}]},{"nodeName":"body","attributes":{"parent":"topic"},"children":[{"nodeName":"section","attributes":{"parent":"body"},"children":[{"nodeName":"p","attributes":{"parent":"section"},"children":[{"nodeName":"text","content":"A test paragraph."}]}]}]}]}]}`;
     cy.visit('http://localhost:1234/')
     .get('#saveFile')
     .click()
