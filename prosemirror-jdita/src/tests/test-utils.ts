9e72a9d5fec3c5be0fff216e2e83a8b391b6741d
marmoure <tinky93winky@gmail.com>
2024-06-04 11:41:50 +0200

    [bugfix] lwdita converter can not handle white spaces between the tags

--- prosemirror-jdita/src/tests/test-utils.ts
+++ prosemirror-jdita/src/tests/test-utils.ts
@@ -17,7 +17,7 @@ export function title(attrs: Record<string, string> = {}, content: string = ''):
 }
 
 export const JDITA_NODE               = '{"nodeName":"title","attributes":{},"children":[{"nodeName":"text","content":"Programming Light Bulbs to a Lighting Group"}]}';
-export const JDITA_PARENT_NODE        = '{"nodeName":"topic","attributes":{"id":"program"},"children":[{"nodeName":"title","attributes":{},"children":[{"nodeName":"text","content":"Programming Light Bulbs to a Lighting Group"}]},{"nodeName":"body","attributes":{},"children":[{"nodeName":"section","attributes":{},"children":[{"nodeName":"p","attributes":{},"children":[{"nodeName":"text","content":"You must assign a light bulb to at least one lighting group to operate that light bulb."}]}]}]}]}';
+ export const JDITA_PARENT_NODE        = '{"nodeName":"topic","attributes":{"id":"program"},"children":[{"nodeName":"title","attributes":{},"children":[{"nodeName":"text","content":"Programming Light Bulbs to a Lighting Group"}]},{"nodeName":"body","attributes":{},"children":[{"nodeName":"section","attributes":{},"children":[{"nodeName":"p","attributes":{},"children":[{"nodeName":"text","content":"You must assign a light bulb to at least one lighting group to operate that light bulb."}]}]}]}]}';
 export const JDITA_TRANFORMED_RESULT1 = '{"type":"title","attrs":{}}';
 export const JDITA_TRANFORMED_RESULT2 = '{"type":"title","attrs":{},"content":[{"type":"text","text":"Programming Light Bulbs to a Lighting Group","attrs":{"parent":"title"}}]}';
 export const XML = `<?xml version="1.0" encoding="UTF-8"?>
@@ -90,128 +90,12 @@ export const JDITA_OBJECT = '{ "nodeName": "document", "children": [ { "nodeName
 
 export const TRANSFORMED_JDITA_OBJECT = '{ "type": "doc", "attrs": {}, "content": [ { "type": "topic", "attrs": { "id": "program", "parent": "doc" }, "content": [ { "type": "title", "attrs": { "parent": "topic" }, "content": [ { "type": "text", "text": "Programming Light Bulbs to a Lighting Group", "attrs": { "parent": "title" } } ] }, { "type": "shortdesc", "attrs": { "parent": "topic" }, "content": [ { "type": "text", "text": "You can program one or more light bulbs.", "attrs": { "parent": "shortdesc" } } ] }, { "type": "body", "attrs": { "parent": "topic" }, "content": [ { "type": "section", "attrs": { "parent": "body" }, "content": [ { "type": "p", "attrs": { "parent": "section" }, "content": [ { "type": "text", "text": "You must assign a light bulb to at least one lighting group to operate that light bulb.", "attrs": { "parent": "p" } } ] } ] } ] } ] } ] }';
 
-export const shortXdita = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
-<topic id="program">
-  <title>Test File 2</title>
-  <body>
-    <section>
-      <p>A test paragraph.</p>
-    </section>
-  </body>
-</topic>`;
+export const shortXdita = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic id="program"><title>Test File 2</title><body><section><p>A test paragraph.</p></section></body></topic>`;
 
-export const complexXdita = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
-<topic id="program-bulbs-to-groups">
-  <title>Test File: Programming Light Bulbs to a Lighting Group</title>
-  <shortdesc>You can program one or more light bulbs to a lighting group to operate that group
-    with your remote control.</shortdesc>
-  <body>
-    <section id="context">
-      <p>Your <ph keyref="product-name"/> remote control can manage up to <data>250 network light bulbs on the same lighting
-      network</data>. When you add a light bulb to the network, you can program it to one or more
-        lighting groups. You must assign a light bulb to at least one lighting group to
-        operate that light bulb  A network light bulb that is not programmed to a
-        lighting group will still operate when controlling all network light bulbs from
-        the remote control.</p>
-    </section>
-    <section id="steps">
-   <ol>
-     <li><p>Make sure your remote control is in range of the light bulbs you are
-        adding.</p></li>
-     <li><p>If a network light bulb is new, you must install it by performing the following
-          steps:</p>
-          <ol>
-            <li><p>Make sure power to the fixture where you are installing the light bulb
-              is turned OFF.</p>
-            </li>
-              <li><p>Remove any existing light bulb from the light fixture.</p></li>
-              <li><p>Install the network light bulb into the light fixture as you would any
-              standard light bulb.</p></li>
-              <li><p>Turn power to the light fixture on.</p>
-              <p>The light bulb begins to brighten and dim while finding the
-              remote control's network.</p></li>
-          <li><p>Repeat steps for each new network light bulb.</p></li>
-          </ol></li>
-        <li><p>Turn power on to the fixtures containing network light bulbs you want added to
-          the light group.</p></li>
-          <li><p>Turn power off to the fixtures containing light bulbs you do not want added to
-          the light group. </p></li>
-          <li><p>On the remote control, press and hold the desired lighting group button for 5
-          seconds.</p>
-        <p>The button indicator for the selected lighting group flashes green while
-          the light bulb(s) are added to the group. If the indicator flashes red, the
-          lighting group was not activated and you must try again. Light flashes red for 3
-          seconds if programming fails.</p>
-      </li>
-      <li><p>Leave the light fixture switches ON so that power is available when using your
-          remote control to turn the light bulbs on and off.</p></li>
-      <li><p>remember to turn on any excluded fixtures that you turned off.</p></li>
-    </ol>
-    </section>
+export const complexXdita = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic id="program-bulbs-to-groups"><title>Test File: Programming Light Bulbs to a Lighting Group</title><shortdesc>You can program one or more light bulbs to a lighting group to operate that group with your remote control.</shortdesc><body><section id="context"><p>Your <ph keyref="product-name"/> remote control can manage up to <data>250 network light bulbs on the same lighting network</data>. When you add a light bulb to the network, you can program it to one or more lighting groups. You must assign a light bulb to at least one lighting group to operate that light bulb  A network light bulb that is not programmed to a lighting group will still operate when controlling all network light bulbs from the remote control.</p></section><section id="steps"><ol><li><p>Make sure your remote control is in range of the light bulbs you are adding.</p></li><li><p>If a network light bulb is new, you must install it by performing the following steps:</p><ol><li><p>Make sure power to the fixture where you are installing the light bulb is turned OFF.</p></li><li><p>Remove any existing light bulb from the light fixture.</p></li><li><p>Install the network light bulb into the light fixture as you would any standard light bulb.</p></li><li><p>Turn power to the light fixture on.</p><p>The light bulb begins to brighten and dim while finding the remote control's network.</p></li><li><p>Repeat steps for each new network light bulb.</p></li></ol></li><li><p>Turn power on to the fixtures containing network light bulbs you want added to the light group.</p></li><li><p>Turn power off to the fixtures containing light bulbs you do not want added to the light group. </p></li><li><p>On the remote control, press and hold the desired lighting group button for 5 seconds.</p><p>The button indicator for the selected lighting group flashes green while the light bulb(s) are added to the group. If the indicator flashes red, the lighting group was not activated and you must try again. Light flashes red for 3 seconds if programming fails.</p></li><li><p>Leave the light fixture switches ON so that power is available when using your remote control to turn the light bulbs on and off.</p></li><li><p>remember to turn on any excluded fixtures that you turned off.</p></li></ol></section></body></topic>`;
 
-  </body>
-</topic>
-`;
+export const videoXdita = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic id="program-bulbs-to-groups"><title>Test File: Media Elements in XDita</title><body><section id="demo"><title>Video Element</title><fig><video xml:lang="en" outputclass="videoElement" width="640" height="360"><desc outputclass="videoElementDescriptor">Xiaomi Yeelight YLDP06YL Smart Light Bulb White</desc><video-poster value="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm/120px--1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm.jpg" /><media-controls value="true"/><media-autoplay value="false"/><media-loop value="false"/><media-muted value="false"/><media-source value="https://commons.wikimedia.org/wiki/File:1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm" /></video></fig></section></body></topic>`;
 
-export const videoXdita = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
-<topic id="program-bulbs-to-groups">
-  <title>Test File: Media Elements in XDita</title>
-  <body>
-    <section id="demo">
-      <title>
-        Video Element
-      </title>
-      <fig>
-        <video xml:lang="en" outputclass="videoElement" width="640" height="360">
-          <desc outputclass="videoElementDescriptor">Xiaomi Yeelight YLDP06YL Smart Light Bulb White</desc>
-          <video-poster value="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm/120px--1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm.jpg" />
-          <media-controls value="true"/>
-          <media-autoplay value="false"/>
-          <media-loop value="false"/>
-          <media-muted value="false"/>
-          <media-source value="https://commons.wikimedia.org/wiki/File:1954_Kool-Aid_Commercial._Debut_of_Pitcher_Man.webm" />
-        </video>
-        </fig>
-    </section>
-  </body>
-</topic>`;
+export const audioXdita = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic id="program-bulbs-to-groups"><title>Test File: Media Elements in XDita</title><body><section><title>Audio Element</title><fig><audio xml:lang="en" outputclass="audioElement"><desc outputclass="audioElementDescriptor">An Audio Track</desc><media-controls value="true"/><media-autoplay value="false"/><media-loop value="false"/><media-muted value="false"/><media-source value="https://commons.wikimedia.org/wiki/File:1943_Nov_14_NYPhil_Bernstein.ogg" /></audio></fig></section></body></topic>`;
 
-export const audioXdita = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
-<topic id="program-bulbs-to-groups">
-  <title>Test File: Media Elements in XDita</title>
-  <body>
-    <section>
-      <title>Audio Element</title>
-      <fig>
-        <audio xml:lang="en" outputclass="audioElement">
-          <desc outputclass="audioElementDescriptor">An Audio Track</desc>
-          <media-controls value="true"/>
-          <media-autoplay value="false"/>
-          <media-loop value="false"/>
-          <media-muted value="false"/>
-          <media-source value="https://commons.wikimedia.org/wiki/File:1943_Nov_14_NYPhil_Bernstein.ogg" />
-        </audio>
-      </fig>
-    </section>
-  </body>
-</topic>`;
-
-export const imageXdita = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
-<topic id="program-bulbs-to-groups">
-  <title>Test File: Media Elements in XDita</title>
-  <body>
-    <section>
-      <title>An Image</title>
-      <fig>
-        <image xml:lang="en" format="test" dir="ltr" outputclass="imageElement" height="300px" width="300px" href="https://placekitten.com/300/300">
-          <alt>A kitten</alt>
-        </image>
-      </fig>
-    </section>
-  </body>
-</topic>`;
+export const imageXdita = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic id="program-bulbs-to-groups"><title>Test File: Media Elements in XDita</title><body><section><title>An Image</title><fig><image xml:lang="en" format="test" dir="ltr" outputclass="imageElement" height="300px" width="300px" href="https://placekitten.com/300/300"><alt>A kitten</alt></image></fig></section></body></topic>`;
