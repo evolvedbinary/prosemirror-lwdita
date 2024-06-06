e6d939542670bbca353d760e5af403c8d60289f5
marmoure <tinky93winky@gmail.com>
2024-06-06 11:42:51 +0200

    [test] update cypress test to download xml

--- prosemirror-lwdita-demo/cypress/e2e/download.cy.ts
+++ prosemirror-lwdita-demo/cypress/e2e/download.cy.ts
@@ -2,20 +2,32 @@ describe('download a file', () => {
 
   it('downloads the file correctly', () => {
     cy.visit('http://localhost:1234/')
-    .readFile('Test_File.json').
+    .readFile('Test_File.xml').
     should('not.exist')
     .get('#saveFile').click()
-    .readFile('cypress/downloads/Test_File.json');
+    .readFile('cypress/downloads/Test_File.xml');
   })
 
-  // TODO: For the time being we are testing a JDITA download.
-  // Once the feature of downloading XDita is completed, change`mockJdita` test data to XDita.
+
   it('downloads the file correctly', () => {
-    const mockJdita = `{"nodeName":"document","attributes":{},"children":[{"nodeName":"topic","attributes":{"parent":"doc","id":"program"},"children":[{"nodeName":"title","attributes":{"parent":"topic"},"children":[{"nodeName":"text","content":"Test File 2"}]},{"nodeName":"body","attributes":{"parent":"topic"},"children":[{"nodeName":"section","attributes":{"parent":"body"},"children":[{"nodeName":"p","attributes":{"parent":"section"},"children":[{"nodeName":"text","content":"A test paragraph."}]}]}]}]}]}`;
+    const mockXML = `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd"><topic parent="doc" id="program">
+    <title parent="topic">
+        Test File 2
+    </title>
+    <body parent="topic">
+        <section parent="body">
+            <p parent="section">
+                A test paragraph.
+            </p>
+        </section>
+    </body>
+</topic>
+`;
     cy.visit('http://localhost:1234/')
     .get('#saveFile')
     .click()
-    .readFile('cypress/downloads/Test_File.json')
-    .should('deep.equal', JSON.parse(mockJdita));
+    .readFile('cypress/downloads/Test_File.xml')
+    .debug()
+    .should('equal', mockXML);
   })
 })
