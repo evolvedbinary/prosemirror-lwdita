ca824e4af0ae93805cd1435c9d585c73d1385236
marmoure <tinky93winky@gmail.com>
2024-07-11 00:18:04 +0200

    [test] cypress test for new line

---
+++ packages/prosemirror-lwdita-demo/cypress/e2e/newline.cy.ts
@@ -0,0 +1,32 @@
+describe('inserts a line break', () => {
+    beforeEach(() => {
+        window.localStorage.setItem('file', `<?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE topic PUBLIC "-//OASIS//DTD LIGHTWEIGHT DITA Topic//EN" "lw-topic.dtd">
+    <topic id="program">
+      <title>Test File 2</title>
+      <body>
+        <section>
+          <p>A test paragraph.</p>
+        </section>
+      </body>
+    </topic>`);
+      })
+  
+  it('can insert a line break in the title', () => {
+    cy.visit('http://localhost:1234/')
+    .get('#editor > div > div.ProseMirror > article > h1')
+    .click()
+    cy.focused()
+    .type('{leftArrow}{enter}title')
+    .should('contain.html', '<br>')
+  })
+
+  it('can insert a line break in the body', () => {
+    cy.visit('http://localhost:1234/')
+    .get('#editor > div > div.ProseMirror > article > div > section > p')
+    .click()
+    cy.focused()
+    .type('{leftArrow}{enter}body')
+    .should('contain.html', '<br>')
+  })
+})
