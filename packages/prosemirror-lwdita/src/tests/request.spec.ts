7945c4de5fda273919e159375e0324572f9db6b8
Alexandra von Criegern <alexandra@evolvedbinary.com>
2024-09-05 12:26:51 +0200

    [feature] Create function `hasParameters()`
    * Create module request.ts with function hasParameters() to check, if a url has parameters or not.
    * Create a unit test for hasParameters()

---
+++ packages/prosemirror-lwdita/src/tests/request.spec.ts
@@ -0,0 +1,39 @@
+/*!
+Copyright (C) 2020 Evolved Binary
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { hasParameters } from '../request';
+import ChaiPromised from 'chai-as-promised';
+import { use, expect } from 'chai';
+
+use(ChaiPromised);
+
+let urlWithoutParameters: string, urlWithParameters : string;
+
+// check if the URL has parameters
+describe.only('Function hasParameters()', () => {
+  urlWithoutParameters = 'https://example.com/';
+  urlWithParameters = 'https://example.com/?ghrepo=repo1&source=source1&referer=referer1';
+
+  it('returns false if the URL has no parameters', () => {
+    expect(hasParameters(urlWithoutParameters)).to.equal(false);
+  });
+
+  it('returns true if the URL has parameters', () => {
+    expect(hasParameters(urlWithParameters)).to.equal(true);
+  });
+})
+
