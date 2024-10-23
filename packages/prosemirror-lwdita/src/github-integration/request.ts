bce0706d4c6a4b99869a6457dc7f0827ff44c32c
marmoure <tinky93winky@gmail.com>
2024-10-23 13:21:21 +0200

    [IGNORE] use pineapple for the server url

--- packages/prosemirror-lwdita/src/github-integration/request.ts
+++ packages/prosemirror-lwdita/src/github-integration/request.ts
@@ -127,7 +127,7 @@ export function redirectToGitHubOAuth(parameters: URLParams): void {
   const { id, value } = clientID;
   // Store the parameters in state to pass them to the redirect URL
   const state = btoa(`${JSON.stringify({ ...parameters })}`);
-  const redirectURL = serverURL.value + '/api/github/auth';
+  const redirectURL = serverURL.value + 'api/github/auth';
   window.location.href = `https://github.com/login/oauth/authorize?${id}=${value}&state=${state}&redirect_uri=${redirectURL}`;
 }
 
